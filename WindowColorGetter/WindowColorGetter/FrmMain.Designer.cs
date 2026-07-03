14a05e426d7b22194dd68bb665fdcee5ab89dab8
kano hayato <kano.hayato@alhinc.jp>
2026-07-03 16:48:18 +0900

    kanoA ベースシステム作成

---
+++ WindowColorGetter/WindowColorGetter/FrmMain.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace WindowColorGetter
+{
+    partial class FrmWindowColorGetter
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnStartGetColorMode = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtboxRGB = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Control;
+            this.label1.Font = new System.Drawing.Font("Yu Gothic UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.label1.Location = new System.Drawing.Point(25, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(461, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "「色取得開始」ボタン押下で、画面上を左クリックして色を取得するモードになります";
+            // 
+            // btnStartGetColorMode
+            // 
+            this.btnStartGetColorMode.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnStartGetColorMode.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStartGetColorMode.Font = new System.Drawing.Font("ＭＳ ゴシック", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.btnStartGetColorMode.Location = new System.Drawing.Point(25, 125);
+            this.btnStartGetColorMode.Name = "btnStartGetColorMode";
+            this.btnStartGetColorMode.Size = new System.Drawing.Size(100, 50);
+            this.btnStartGetColorMode.TabIndex = 1;
+            this.btnStartGetColorMode.TabStop = false;
+            this.btnStartGetColorMode.Text = "色取得開始";
+            this.btnStartGetColorMode.UseVisualStyleBackColor = false;
+            this.btnStartGetColorMode.Click += new System.EventHandler(this.btnStartGetColorMode_Click);
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.SystemColors.Control;
+            this.label2.Font = new System.Drawing.Font("Yu Gothic UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.label2.Location = new System.Drawing.Point(25, 275);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "R, B, G";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // txtboxRGB
+            // 
+            this.txtboxRGB.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtboxRGB.Cursor = System.Windows.Forms.Cursors.Default;
+            this.txtboxRGB.Font = new System.Drawing.Font("Yu Gothic UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.txtboxRGB.Location = new System.Drawing.Point(106, 272);
+            this.txtboxRGB.Name = "txtboxRGB";
+            this.txtboxRGB.ReadOnly = true;
+            this.txtboxRGB.Size = new System.Drawing.Size(100, 26);
+            this.txtboxRGB.TabIndex = 3;
+            this.txtboxRGB.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // FrmWindowColorGetter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(734, 461);
+            this.Controls.Add(this.txtboxRGB);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnStartGetColorMode);
+            this.Controls.Add(this.label1);
+            this.MinimumSize = new System.Drawing.Size(525, 375);
+            this.Name = "FrmWindowColorGetter";
+            this.Text = "画面色取得";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnStartGetColorMode;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtboxRGB;
+    }
+}
+
---
+++ WindowColorGetter/WindowColorGetter/FrmMain.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WindowColorGetter
+{
+    public partial class FrmWindowColorGetter : Form
+    {
+        #region 変数
+        private Color lastColor;
+        #endregion 変数
+
+        #region コンストラクタ
+        public FrmWindowColorGetter()
+        {
+            InitializeComponent();
+        }
+        #endregion コンストラクタ
+
+        #region イベントハンドラ
+        /// <summary>
+        /// クリックイベント - 座標取得開始モードボタン
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnStartGetColorMode_Click(object sender, EventArgs e)
+        {
+            // 色取得用の透明ウィンドウを作成
+            Form frmOverlay = new Form();
+
+            frmOverlay.FormBorderStyle = FormBorderStyle.None;
+            frmOverlay.ShowInTaskbar = false;
+            frmOverlay.TopMost = true;
+            frmOverlay.BackColor = Color.White;
+            frmOverlay.Opacity = 0.01;
+
+            // 複数ディスプレイ向けにフォーム配置
+            Rectangle totalArea = Rectangle.Empty;
+            foreach (var screen in Screen.AllScreens)
+            {
+                totalArea = Rectangle.Union(totalArea, screen.Bounds);
+            }
+            frmOverlay.Bounds = totalArea;
+
+            frmOverlay.Click += OverlayForm_Click;
+            frmOverlay.Show();
+        }
+
+        /// <summary>
+        /// クリックイベント - 座標取得用透明フォーム
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OverlayForm_Click(object sender, EventArgs e)
+        {
+            if (sender is Form frm) frm.Close();
+            // 座標取得
+            Point clickPoint = Cursor.Position;
+            // 座標の画面上色取得
+            lastColor = GetColorAtPoint(clickPoint);
+
+            Debug.WriteLine(clickPoint + ", " + lastColor);
+
+            // RGB値設定
+            string strRGBColor = this.lastColor.R.ToString() + "," + this.lastColor.G.ToString() + "," + this.lastColor.B.ToString();
+            this.txtboxRGB.Text = strRGBColor;
+        }
+        #endregion イベントハンドラ
+
+        #region メソッド
+        /// <summary>
+        /// 座標上の色を取得
+        /// </summary>
+        /// <param name="screenPoint">クリックした座標</param>
+        /// <returns>Color:座標の色</returns>
+        private Color GetColorAtPoint(Point screenPoint)
+        {
+            Color result = Color.Empty;
+
+            // カーソル先画像用の1×1ビットマップ
+            using (Bitmap bmp = new Bitmap(1, 1))
+            {
+                using (Graphics g = Graphics.FromImage(bmp))
+                {
+                    g.CopyFromScreen(screenPoint, Point.Empty, new Size(1, 1));
+                }
+                // 画像の色取得
+                result = bmp.GetPixel(0, 0);
+            }
+            return result;
+        }
+        #endregion メソッド
+    }
+}
